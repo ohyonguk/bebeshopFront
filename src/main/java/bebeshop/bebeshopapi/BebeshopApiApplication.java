073ef296b2a9dbce5c334260bb3d99fa560b2996
ohyonguk <oksik68@naver.com>
2024-06-10 22:57:14 +0900

    6/10

--- src/main/java/bebeshop/bebeshopapi/BebeshopApiApplication.java
+++ src/main/java/bebeshop/bebeshopapi/BebeshopApiApplication.java
@@ -1,11 +1,12 @@
 package bebeshop.bebeshopapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class BebeshopApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BebeshopApiApplication.class, args);
     }
